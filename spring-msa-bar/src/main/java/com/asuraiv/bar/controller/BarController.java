2f31039a4a2350365bc78ff85ff3c1e28281fc89
asuraiv <asuraiv7@gmail.com>
2020-01-04 22:34:03 +0900

    zipkin client와 sleuth의 autoconfigure 설정

--- spring-msa-bar/src/main/java/com/asuraiv/bar/controller/BarController.java
+++ spring-msa-bar/src/main/java/com/asuraiv/bar/controller/BarController.java
@@ -1,13 +1,18 @@
 package com.asuraiv.bar.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class BarController {
 
-	@GetMapping("/bar")
-	public String bar() {
-		return "bar";
+	@GetMapping("/bar/receive-a-greet")
+	public String greeting() {
+
+		log.info("Bar server greeting handler is called.");
+
+		return "Thank you. Foo";
 	}
 }
--- spring-msa-foo/src/main/java/com/asuraiv/foo/SpringMsaFooApplication.java
+++ spring-msa-foo/src/main/java/com/asuraiv/foo/SpringMsaFooApplication.java
@@ -2,9 +2,16 @@ package com.asuraiv.foo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class SpringMsaFooApplication {
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringMsaFooApplication.class, args);
